4cd681ab29a3ccda0fe2b81c05a202ba3203fb70
Sonia <s.camacho.her@gmail.com>
2018-05-14 16:52:19 +0200

    Nothing important yet

--- style_transfer.py
+++ style_transfer.py
@@ -1,6 +1,7 @@
 import cv2
 import sys
 import argparse
+import tensorflow as tf
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='Style transfer.')
@@ -16,23 +17,34 @@ def checkImage(img, path):
 	if img is None:
 		sys.exit("Error: Invalid path {}\nExiting the program.\n".format(path))
 
+def read_image(path):
+	img = cv2.imread(path)
+	checkImage(img, path)
+	#Resize images?
+	return img
+
 ########## MAIN FUNCTION ##########
 global args
 args = parse_args()
 
+# Load weights
+
+# Normalize weights
+
+
 #Read Images
 content_path = 'content-img/' + args.content_img
-content_img = cv2.imread(content_path)
-checkImage(content_img, content_path)
+content_img = read_image(content_path)
 
 style_path = 'style-img/' + args.style_img
-style_img = cv2.imread(style_path)
-checkImage(style_img, style_path)
+style_img = read_image(style_path)
 
 #Display Image (this is just for testing)
-cv2.imshow('image',style_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.imshow('image',style_img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+#Do the magic
 
 #Save result
 cv2.imwrite('result/result.png',style_img)
