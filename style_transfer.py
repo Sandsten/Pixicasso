8f2e156f2cb116e869abd99c9e489ab06b1d1a20
Sonia <s.camacho.her@gmail.com>
2018-05-15 14:17:58 +0200

    More simplifications and corrections

--- style_transfer.py
+++ style_transfer.py
@@ -78,7 +78,7 @@ def load_vgg_model(path, input_image):
     vgg = sio.loadmat(path)
     vgg_layers = vgg['layers']
 
-    def _weights(layer, expected_layer_name):
+    def get_weights(layer, expected_layer_name):
         """
         Return the weights and bias from the VGG model for a given layer.
         """
@@ -89,19 +89,19 @@ def load_vgg_model(path, input_image):
         assert layer_name == expected_layer_name
         return W, b
 
-    def _conv2d_relu(prev_layer, layer, layer_name):
+    def conv2d_relu(prev_layer, layer, layer_name):
         """
         Return the Conv2D + RELU layer using the weights, biases from the VGG
         model at 'layer'.
         """
-        W, b = _weights(layer, layer_name)
+        W, b = get_weights(layer, layer_name)
         W = tf.constant(W)
         b = tf.constant(np.reshape(b, (b.size)))
         return tf.nn.relu(tf.nn.conv2d(
             prev_layer, filter=W, strides=[1, 1, 1, 1], padding='SAME') + b)
 
 
-    def _avgpool(prev_layer):
+    def avgpool(prev_layer):
         """
         Return the AveragePooling layer.
         """
@@ -111,27 +111,27 @@ def load_vgg_model(path, input_image):
     graph = {}
     _, h, w, d     = input_image.shape
     graph['input']   = tf.Variable(np.zeros((1, h, w, d)), dtype = 'float32')
-    graph['conv1_1']  = _conv2d_relu(graph['input'], 0, 'conv1_1')
-    graph['conv1_2']  = _conv2d_relu(graph['conv1_1'], 2, 'conv1_2')
-    graph['avgpool1'] = _avgpool(graph['conv1_2'])
-    graph['conv2_1']  = _conv2d_relu(graph['avgpool1'], 5, 'conv2_1')
-    graph['conv2_2']  = _conv2d_relu(graph['conv2_1'], 7, 'conv2_2')
-    graph['avgpool2'] = _avgpool(graph['conv2_2'])
-    graph['conv3_1']  = _conv2d_relu(graph['avgpool2'], 10, 'conv3_1')
-    graph['conv3_2']  = _conv2d_relu(graph['conv3_1'], 12, 'conv3_2')
-    graph['conv3_3']  = _conv2d_relu(graph['conv3_2'], 14, 'conv3_3')
-    graph['conv3_4']  = _conv2d_relu(graph['conv3_3'], 16, 'conv3_4')
-    graph['avgpool3'] = _avgpool(graph['conv3_4'])
-    graph['conv4_1']  = _conv2d_relu(graph['avgpool3'], 19, 'conv4_1')
-    graph['conv4_2']  = _conv2d_relu(graph['conv4_1'], 21, 'conv4_2')
-    graph['conv4_3']  = _conv2d_relu(graph['conv4_2'], 23, 'conv4_3')
-    graph['conv4_4']  = _conv2d_relu(graph['conv4_3'], 25, 'conv4_4')
-    graph['avgpool4'] = _avgpool(graph['conv4_4'])
-    graph['conv5_1']  = _conv2d_relu(graph['avgpool4'], 28, 'conv5_1')
-    graph['conv5_2']  = _conv2d_relu(graph['conv5_1'], 30, 'conv5_2')
-    graph['conv5_3']  = _conv2d_relu(graph['conv5_2'], 32, 'conv5_3')
-    graph['conv5_4']  = _conv2d_relu(graph['conv5_3'], 34, 'conv5_4')
-    graph['avgpool5'] = _avgpool(graph['conv5_4'])
+    graph['conv1_1']  = conv2d_relu(graph['input'], 0, 'conv1_1')
+    graph['conv1_2']  = conv2d_relu(graph['conv1_1'], 2, 'conv1_2')
+    graph['avgpool1'] = avgpool(graph['conv1_2'])
+    graph['conv2_1']  = conv2d_relu(graph['avgpool1'], 5, 'conv2_1')
+    graph['conv2_2']  = conv2d_relu(graph['conv2_1'], 7, 'conv2_2')
+    graph['avgpool2'] = avgpool(graph['conv2_2'])
+    graph['conv3_1']  = conv2d_relu(graph['avgpool2'], 10, 'conv3_1')
+    graph['conv3_2']  = conv2d_relu(graph['conv3_1'], 12, 'conv3_2')
+    graph['conv3_3']  = conv2d_relu(graph['conv3_2'], 14, 'conv3_3')
+    graph['conv3_4']  = conv2d_relu(graph['conv3_3'], 16, 'conv3_4')
+    graph['avgpool3'] = avgpool(graph['conv3_4'])
+    graph['conv4_1']  = conv2d_relu(graph['avgpool3'], 19, 'conv4_1')
+    graph['conv4_2']  = conv2d_relu(graph['conv4_1'], 21, 'conv4_2')
+    graph['conv4_3']  = conv2d_relu(graph['conv4_2'], 23, 'conv4_3')
+    graph['conv4_4']  = conv2d_relu(graph['conv4_3'], 25, 'conv4_4')
+    graph['avgpool4'] = avgpool(graph['conv4_4'])
+    graph['conv5_1']  = conv2d_relu(graph['avgpool4'], 28, 'conv5_1')
+    graph['conv5_2']  = conv2d_relu(graph['conv5_1'], 30, 'conv5_2')
+    graph['conv5_3']  = conv2d_relu(graph['conv5_2'], 32, 'conv5_3')
+    graph['conv5_4']  = conv2d_relu(graph['conv5_3'], 34, 'conv5_4')
+    graph['avgpool5'] = avgpool(graph['conv5_4'])
     return graph
 
 
@@ -204,8 +204,6 @@ def style_loss_func(sess, model):
 global args
 args = parse_args()
 
-sess = tf.InteractiveSession()
-
 #Read Images
 content_path = 'content-img/' + args.content_img
 content_img = read_content_image(content_path)
@@ -216,6 +214,7 @@ style_img = read_style_image(style_path, content_img)
 #input_image = generate_noise_image(content_img)
 input_image = content_img
 
+sess = tf.InteractiveSession()
 model = load_vgg_model(VGG_MODEL, input_image)
 
 #Display Image (this is just for testing)
@@ -243,9 +242,6 @@ total_loss = BETA * content_loss + ALPHA * style_loss
 # layers. Then we minimize the total_loss, which is the equation 7.
 optimizer = tf.train.AdamOptimizer(2.0)
 train_step = optimizer.minimize(total_loss)
-
-sess.run(tf.initialize_all_variables())
-sess.run(model['input'].assign(input_image))
 
 sess.run(tf.initialize_all_variables())
 sess.run(model['input'].assign(input_image))
